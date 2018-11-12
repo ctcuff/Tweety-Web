f1e39a3df0478bc672cf3a1ccb91f0ca079d5448
Cameron Cuff <dev.ctcuff@gmail.com>
2018-11-12 15:44:31 -0500

    disable start/stop buttons if stream is running

--- stream.js
+++ stream.js
@@ -4,12 +4,29 @@ const dialog = require('electron').remote.dialog;
 const $gridContainer = $('.flex-container');
 const $occurrences = $('#occurrences');
 const $input = $('#input-keyword');
-
+const $start = $('#start');
+const $stop = $('#stop');
 const T = new Twit(config);
 
 let stream;
 let hasStarted = false;
-let occurrences = 0;
+let numOccurrences = 0;
+let isAtBottom = true;
+
+// Start the stream when 'enter' is pressed
+$(document).ready(function() {
+  $input.keypress(function(event) {
+    if (event.keyCode === 13) {
+      startStream();
+    }
+  });
+});
+
+// Only scroll to the bottomof the page if the page is
+// already at the bottom
+window.onscroll = () => {
+  isAtBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight;
+};
 
 function startStream() {
   let keywords = [];
@@ -24,12 +41,15 @@ function startStream() {
     keywords.push(word);
   });
 
-  console.log(`Stream started -> [${keywords}]`);
-
+  // Disable the start button but enable the stop button
+  $start.attr('disabled', 'disabled');
+  $stop.removeAttr('disabled');
   $input.attr('placeholder', `Keyword(s): ${keywords}`);
   
   // Clear the input field
   $input.val('');
+ 
+  console.log(`Stream started -> [${keywords}]`);
 
   if (!hasStarted) {
     stream = T.stream('statuses/filter', {track: keywords});
@@ -37,8 +57,11 @@ function startStream() {
 
   stream.on('tweet', tweet => {
     addCard(tweet);
-    console.log(`@${tweet.user.screen_name} => [${JSON.stringify(tweet)}]`);
-    $occurrences.text(++occurrences);
+    $occurrences.text(++numOccurrences);
+
+    if (isAtBottom) { 
+      window.scrollTo(0, document.body.scrollHeight);
+    }
   });
 
   hasStarted = true; 
@@ -51,7 +74,7 @@ function addCard(tweet) {
   // (Twitter returns it's dates as: Thu Jul 25 22:46:29 +0000 2018) 
   let date = tweet.created_at.slice(0, tweet.created_at.length - 11);
 
-  const $cardContainer = $("<div class='card'></div>");
+  const $cardContainer = $("<div class='card shadow-lg rounded'></div>");
   const $cardBody = $(`<div class='card-body'></div>`);
   const $cardTitle = $(`<h5 class='card-title' id='tweet-username'>@${tweet.user.screen_name}</h5>`);
   const $cardSubtitle = $(`<h6 class="card-subtitle mb-2 text-muted" id="tweet-date">${date}</h6>`);
@@ -66,14 +89,24 @@ function addCard(tweet) {
   $cardContainer.append($cardBody)
 
   $gridContainer.append($cardContainer);
+
+  $cardContainer.click(function() {
+    console.log(`@${tweet.user.screen_name} => [${JSON.stringify(tweet)}]`);
+  });
 }
 
 function stopStream() {
   if (hasStarted) {
     console.log('Stream stopped');
+
     stream.stop();
+    
+    // Re-enable the start button but diable the stop button
+    $start.removeAttr('disabled');
+    $stop.attr('disabled', 'disabled');
+    $input.attr('placeholder', 'Keyword(s):');
   }
   hasStarted = false;
-  occurrences = 0;
-  $occurrence.text(occurrences);
+  numOccurrences = 0;
+  $occurrences.text(numOccurrences);
 }
